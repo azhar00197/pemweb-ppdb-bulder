4f45ac5c641410bf255e0e2e63328eef5cf74a32
fikrimus35 <mustofa13.fikri@gmail.com>
2021-04-16 23:47:02 +0000

    Create overview of landing page
    Signed-off-by: fikrimus35 <mustofa13.fikri@gmail.com>

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PPDB Builder</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+  <nav>
+    <div class="brand">PPDB <span>Builder</span></div>
+    <div class="menu">
+      <a class="menu-item active">OVERVIEW</a>
+      <a class="menu-item">FEATURES</a>
+    </div>
+  </nav>
+  <section class="section-overview">
+    <a href="#" class="main-btn">BUILD NOW</a>
+  </section>
+</body>
+
+</html>
